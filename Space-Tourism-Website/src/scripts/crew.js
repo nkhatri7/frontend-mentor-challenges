4e3b3bc8066e4ba971cc215dd991057074722b40
nkhatri7 <79176897+nkhatri7@users.noreply.github.com>
2021-11-04 15:00:25 +1100

    Added swipe feature for crew page

--- Space-Tourism-Website/src/scripts/crew.js
+++ Space-Tourism-Website/src/scripts/crew.js
@@ -7,17 +7,19 @@ window.addEventListener('load', () => {
     const bioElement = document.getElementById('bio');
     const imgElement = document.getElementById('crew-img');
 
-    options.forEach(async (option) => {
-        let data = await getData();
+    if (window.screen.width.valueOf() <= 768) {
+        handleSwipeEvent();
+    }
 
-        option.addEventListener('click', updateContent);
+    options.forEach(option => {
+        option.addEventListener('click', handleClick);
 
         // Make Douglas Hurley the default view when user goes to crew tab
         if (option.dataset.name === 'Douglas Hurley') {
             option.click();
         }
 
-        function updateContent() {
+        function handleClick() {
             // Update active CSS style
             options.forEach(option => {
                 option.classList.remove('active-option');
@@ -25,18 +27,94 @@ window.addEventListener('load', () => {
             option.classList.add('active-option');
 
             const index = option.dataset.index;
-
-            // Get crew details from JSON file
-            const role = data.crew[index].role;
-            const name = data.crew[index].name;
-            const bio = data.crew[index].bio;
-            const img = data.crew[index].images.webp;
-
-            // Update DOM elements
-            roleElement.innerText = `${role.toUpperCase()}`;
-            nameElement.innerText = `${name.toUpperCase()}`;
-            bioElement.innerText = `${bio}`;
-            imgElement.src = `${img}`;
+            updateContent(index);
         }
     });
+
+    function handleSwipeEvent() {
+        // Lifted from https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        var xDown = null;                                                        
+        var yDown = null;
+
+        let index = 0;
+
+        function getTouches(evt) {
+            return evt.touches || evt.originalEvent.touches;
+        }
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];                                      
+            xDown = firstTouch.clientX;                                      
+            yDown = firstTouch.clientY;                                      
+        }
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+        
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+        
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+                                                                                
+            if (Math.abs( xDiff ) > Math.abs( yDiff )) {
+                if ( xDiff > 0 ) {
+                    // right swipe
+                    if (index <= 2) {
+                        index++;
+                    }
+
+                    // Update active CSS style
+                    options.forEach(option => {
+                        option.classList.remove('active-option');
+                        if (parseInt(option.dataset.index) === index) {
+                            option.classList.add('active-option');
+                        }
+                    });
+
+                    updateContent(index);
+                } else {
+                    // left swipe
+                    if (index > 0) {
+                        index--;
+                    }
+
+                    // Update active CSS style
+                    options.forEach(option => {
+                        option.classList.remove('active-option');
+                        if (parseInt(option.dataset.index) === index) {
+                            option.classList.add('active-option');
+                        }
+                    });
+
+                    updateContent(index);
+                }                       
+            }
+            xDown = null;
+            yDown = null;                                             
+        }
+
+        updateContent(index);
+    }
+
+    async function updateContent(index) {
+        let data = await getData();
+
+        // Get crew details from JSON file
+        const role = data.crew[index].role;
+        const name = data.crew[index].name;
+        const bio = data.crew[index].bio;
+        const img = data.crew[index].images.webp;
+
+        // Update DOM elements
+        roleElement.innerText = `${role.toUpperCase()}`;
+        nameElement.innerText = `${name.toUpperCase()}`;
+        bioElement.innerText = `${bio}`;
+        imgElement.src = `${img}`;
+    }
 });
