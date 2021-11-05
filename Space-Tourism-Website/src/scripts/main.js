7bce80dfbdce7d4ec9b48542997aa4c7d46d2199
nkhatri7 <79176897+nkhatri7@users.noreply.github.com>
2021-11-05 17:33:07 +1100

    Completed space tourism website challenge
    Finished mobile navigation functionality and completed challenge

--- Space-Tourism-Website/src/scripts/main.js
+++ Space-Tourism-Website/src/scripts/main.js
@@ -1,3 +1,16 @@
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+const websiteNav = document.querySelector('.website-nav');
+
+mobileNavToggle.addEventListener('click', () => {
+    const visiblity = websiteNav.getAttribute('data-mobile-open');
+    if (visiblity === "false") {
+        websiteNav.setAttribute('data-mobile-open', true);
+    } else {
+        websiteNav.setAttribute('data-mobile-open', false);
+    }
+    mobileNavToggle.classList.toggle('mobile-nav-open');
+});
+
 export async function getData() {
     const response = await fetch('../../data.json');
     const data = await response.json();
