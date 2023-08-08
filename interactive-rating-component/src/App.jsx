076989bcfbeee988699e91df2a014cb4a84ab72e
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-08-08 20:43:59 +1000

    wip(interactive-rating-component): initialise project

---
+++ interactive-rating-component/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.scss";
+
+const App = () => {
+  const [rating, setRating] = useState(0);
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default App;
