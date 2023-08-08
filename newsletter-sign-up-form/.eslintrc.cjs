592d9648916aa3edeb23c2699f22cf9d57c8c1cf
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-08-08 18:10:58 +1000

    feat: initialise project for newsletter sign up form

---
+++ newsletter-sign-up-form/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "no-trailing-spaces": "warn",
+    "quotes": "warn",
+    "semi": "warn",
+    "no-extra-semi": "warn",
+    "prefer-const": "warn",
+    "max-params": ["warn", 5],
+    "no-duplicate-imports": "warn",
+    "array-bracket-spacing": "warn",
+    "arrow-spacing": "warn",
+    "indent": ["warn", 2],
+    "key-spacing": "warn",
+    "no-multi-spaces": "warn",
+    "object-curly-spacing": ["warn", "always"]
+  },
+}
